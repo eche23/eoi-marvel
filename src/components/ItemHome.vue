a58eef17f942f9b2e4eea59ffada113aa17e6f80
eche23 <eche2312@gmail.com>
2019-08-16 15:56:38 +0100

    create home view

---
+++ src/components/ItemHome.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="item-home">
+    <a href="#"><img class="item-photo" src="../res/img/marvel.jpg" alt=""></a>
+    <a href=""><h3 class="item-title">TITLE</h3></a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ItemHome',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.item-home{
+    display: flex;
+    flex: 1;
+    margin: 10px;
+    flex-direction: column;
+    align-items: center;
+    
+}
+
+a{
+    text-decoration: none;
+}
+
+.item-title{
+    color: #FFFFFF;
+}
+
+.item-photo{
+    display: flex;
+    flex: 1;
+    height: 280px;
+    width: 200px;
+
+}
+
+</style>
---
+++ src/components/ListHome.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="list-home">
+    <ItemHome/>
+    <ItemHome/>
+    <ItemHome/>
+    <ItemHome/>
+    <ItemHome/>
+  </div>
+</template>
+
+
+
+<script>
+
+import ItemHome from '@/components/ItemHome.vue'
+
+export default {
+  name: 'ListHome',
+  props: {
+    msg: String
+  },
+  components: {
+    ItemHome
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.list-home{
+  height: 350px;
+  display: flex;
+  justify-content: space-around;
+  border: #F21826 solid;
+  border-radius: 5px;
+}
+
+</style>
