3309493da6037cf58d17b9bd8435f73a9f649054
eche23 <eche2312@gmail.com>
2019-08-19 15:52:39 +0100

    change method that load item in home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home">
     <a href="#/comics/"><h2 class="home-category">Comics</h2></a>
-    <ListHome :list="comicsHome"/>
+    <ListHome :list="comics"/>
     <a href="#/comics/"><p class="home-more">+more</p></a>
     <a href="#/characters/"><h2 class="home-category">Characters</h2></a>
-    <ListHome :list="charactersHome"/>
+    <ListHome :list="characters"/>
     <a href="#/characterers"><p class="home-more">+more</p></a>
     <a href="#/series/"><h2 class="home-category">Series</h2></a>
-    <ListHome :list="seriesHome"/>
+    <ListHome :list="series"/>
     <a href="#/series/"><p class="home-more">+more</p></a>
   </div>
 </template>
@@ -32,21 +32,18 @@ export default {
     this.index = parseInt(Math.random()*100);
     console.log(this.index);
     
-    Vue.axios.get(data.url+"/v1/public/characters?ts=1&apikey="+data.publicKey+"&hash="+data.hash+"&limit=100").then(res => {
+    Vue.axios.get(data.url+"/v1/public/characters?ts=1&apikey="+data.publicKey+"&hash="+data.hash+"&limit=5&offset="+this.index).then(res => {
       this.characters = res.data;
       this.characters = this.characters.data.results;
-      this.charactersHome = this.characters.slice(this.index, this.index+5);
     })
-    Vue.axios.get(data.url+"/v1/public/comics?ts=1&apikey="+data.publicKey+"&hash="+data.hash+"&limit=100").then(res => {
+    Vue.axios.get(data.url+"/v1/public/comics?ts=1&apikey="+data.publicKey+"&hash="+data.hash+"&limit=5&offset="+this.index).then(res => {
       this.comics = res.data;
       this.comics = this.comics.data.results;
-      this.comicsHome = this.comics.slice(this.index, this.index+5);
       
     })
-    Vue.axios.get(data.url+"/v1/public/series?ts=1&apikey="+data.publicKey+"&hash="+data.hash+"&limit=100").then(res => {
+    Vue.axios.get(data.url+"/v1/public/series?ts=1&apikey="+data.publicKey+"&hash="+data.hash+"&limit=5&offset="+this.index).then(res => {
       this.series = res.data;
       this.series = this.series.data.results;
-      this.seriesHome = this.series.slice(this.index, this.index+5);
     })
   },
   data(){
@@ -54,9 +51,6 @@ export default {
       comics: [],
       series: [],
       characters: [],
-      comicsHome: [],
-      charactersHome: [],
-      seriesHome: [],
       data: data,
       index: 0
     }
